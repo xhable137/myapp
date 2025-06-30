1220c6c1c8faa555498844379cdc87d3dc4efbc8
xhable137 <mr.adarmanyan@mail.ru>
2025-06-30 12:37:37 +0300

    person was updated

--- pkg/models/person.go
+++ pkg/models/person.go
@@ -1,6 +1,15 @@
 package models
 
-type person struct {
+type Person struct {
 	Name string
 	Age  int
+}
+
+func (p *Person) Greet() string {
+	return "Hello, my name is " + p.Name
+}
+
+func (p *Person) HaveBirthday() int {
+	p.Age++
+	return p.Age
 }
